d286fedab8798d759a149b94a766314f493afc2e
Atyam Jayita <jayita.atyam1808@gmail.com>
2025-07-17 20:51:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/All Palindromic Partitions/all-palindromic-partitions.java
+++ Difficulty: Medium/All Palindromic Partitions/all-palindromic-partitions.java
@@ -1,27 +1,32 @@
 class Solution {
-    public ArrayList<ArrayList<String>> palinParts(String s) {
-        // code here
-        ArrayList<ArrayList<String>> al=new ArrayList<>();
-        rec(0,s,al,new ArrayList<>(),new StringBuilder(),0);
-        return al;
-        
+    private boolean checkPalindrome(String s)
+    {
+        if(s.length()==1)return true;
+        StringBuilder ns = new StringBuilder(s);
+        return ns.reverse().toString().equals(s);
     }
-    private void rec(int id,String s,ArrayList<ArrayList<String>> al,ArrayList<String> a,StringBuilder sb,int characters){
-        if(id==s.length()){
-            if(characters==s.length()) al.add(new ArrayList<>(a));
+    public void solve(String s,int i,ArrayList<ArrayList<String>>res,ArrayList<String>str)
+    {
+        if(i>=s.length())
+        {
+            res.add(new ArrayList<>(str));
             return;
         }
-        sb.append(s.charAt(id));
-        if(palin(sb.toString())){
-            a.add(new String(sb.toString()));
-            rec(id+1,s,al,a,new StringBuilder(),characters+sb.length());
-            a.remove(a.size()-1);
+        for(int idx = i+1;idx<=s.length();idx++)
+        {
+            if(checkPalindrome(s.substring(i,idx)))
+            {
+                str.add(s.substring(i,idx));
+                solve(s,idx,res,str);
+                str.remove(str.size()-1);
+            }
         }
-        rec(id+1,s,al,a,sb,characters);
     }
-    private boolean palin(String s){
-        int l=0,r=s.length()-1;
-        while(l<r) if(s.charAt(l++)!=s.charAt(r--)) return false;
-        return true;
+    public ArrayList<ArrayList<String>> palinParts(String s) {
+        // code here
+        ArrayList<ArrayList<String>>res = new ArrayList<>();
+        ArrayList<String>str = new ArrayList<>();
+        solve(s,0,res,str);
+        return res;
     }
 }
