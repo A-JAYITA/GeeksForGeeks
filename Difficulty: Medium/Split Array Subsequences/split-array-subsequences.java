d16888c390185f48481f9063fbdb5edea79f6d35
Atyam Jayita <jayita.atyam1808@gmail.com>
2025-10-24 17:40:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Split Array Subsequences/split-array-subsequences.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    public static boolean isPossible(int[] arr, int k) {
+        // Code here
+        
+        Map<Integer, Integer> count = new HashMap<>();
+        Map<Integer, Integer> end = new HashMap<>();
+
+        for (int num : arr) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : arr) {
+            if (count.get(num) == 0) continue; 
+
+            count.put(num, count.get(num) - 1);
+
+            if (end.getOrDefault(num - 1, 0) > 0) {
+                end.put(num - 1, end.get(num - 1) - 1);
+                end.put(num, end.getOrDefault(num, 0) + 1);
+            }
+            
+            else {
+                boolean canForm = true;
+                for (int j = 1; j < k; j++) {
+                    if (count.getOrDefault(num + j, 0) <= 0) {
+                        canForm = false;
+                        break;
+                    }
+                }
+                if (!canForm) return false;
+
+                for (int j = 1; j < k; j++) {
+                    count.put(num + j, count.get(num + j) - 1);
+                }
+
+                end.put(num + k - 1, end.getOrDefault(num + k - 1, 0) + 1);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 2, 3, 3, 4, 5};
+        int k1 = 2;
+        System.out.println(isPossible(arr1, k1)); 
+
+        int[] arr2 = {1, 1, 1, 1, 1};
+        int k2 = 4;
+        System.out.println(isPossible(arr2, k2));
+    }
+}
