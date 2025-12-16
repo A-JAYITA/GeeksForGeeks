e3c8f78f6e51c32720ea9b811c74a5d6d6fd75f7
Atyam Jayita <jayita.atyam1808@gmail.com>
2025-12-16 09:44:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean areRotations(String s1, String s2) {
+        s1 = s1 + s1;
+        int i = 0;
+        int j = 0;
+        while(i < s1.length() && j < s2.length()){
+            if(s1.charAt(i) == s2.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        if(j == s2.length()) return true;
+        return false;
+    }
+}
