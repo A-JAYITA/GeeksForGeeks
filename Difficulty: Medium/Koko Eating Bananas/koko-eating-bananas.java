6231fc2cdb054ee0722c61d2ae7a50cf9826b9ca
Atyam Jayita <jayita.atyam1808@gmail.com>
2026-02-10 22:46:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
@@ -1,35 +1,39 @@
 class Solution {
-    public static boolean canFinish(int[] arr, int k, int s){
-        int hours=0;
-        for(int val:arr){
-            hours+=(val+s-1)/s;
-        }
-        return hours<=k;
-    }
-     public static int getmax(int[] arr){
-          int max=arr[0];
-          for(int val:arr){
-              if(val>max){
-                  max=val;
-              }
-          }
-          return max;
-     }
     public int kokoEat(int[] arr, int k) {
-        // code here
-        int left=1;
-        int right=getmax(arr);
-        int answer=right;
-      while(left<=right){
-          int mid=left+(right-left)/2;
-          if(canFinish(arr,k,mid)){
-              answer=mid;
-              right=mid-1;
-          }
-          else{
-              left=mid+1;
-          }
-      }
-      return answer;
-      }
+        int n = arr.length;
+        int minTime = findMax(arr);
+        int low = 0, high = minTime;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(mid==0){
+                low = mid+1;
+                continue;
+            }
+            if(didEatAll(arr,k,mid)){
+                minTime = Math.min(minTime, mid);
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return minTime;
+    }
+    int findMax(int [] arr){
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>ans) ans = arr[i];
+        }
+        return ans;
+    }
+    boolean didEatAll(int[] arr, int k, int minHours) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+            sum += ((arr[i] + minHours - 1) / minHours);
+        if (sum > k) return false;
+    }
+    return true;
+}  
+
+    
 }
