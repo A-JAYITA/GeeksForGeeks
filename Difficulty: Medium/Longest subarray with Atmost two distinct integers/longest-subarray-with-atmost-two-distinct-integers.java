e680755342ef20293e3e74d84f2cd328992e8c02
Atyam Jayita <jayita.atyam1808@gmail.com>
2026-03-03 12:56:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.java
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.java
@@ -1,37 +1,19 @@
 class Solution {
     public int totalElements(int[] arr) {
-            int i=0;
-            int j=0;
-            int n=arr.length;
-            int ans=0;
-            HashMap<Integer,Integer> map=new HashMap<>();
-            while(j<n){
-                map.put(arr[j], map.getOrDefault(arr[j],0)+1);
-                if(map.size()<2){
-                    j++;
-                }else if(map.size()==2){
-                    int k=j-i+1;
-                    ans=Math.max(ans,k);
-                    j++;
-                }else {
-                    while(i<j && j<n && map.size()>2){
-                        int temp=arr[i];
-                        if(map.get(temp)>1){
-                            map.put(temp, map.get(temp)-1);
-                        }else {
-                            map.remove(temp);
-                        }
-                        i++;
-                    }
-                    
-                    if(map.size()==2){
-                        int k=j-i+1;
-                        ans=Math.max(ans,k);
-                    }
-                    j++;
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int left = 0;
+        for(int right = 0; right < arr.length ; right++){
+            map.put(arr[right], map.getOrDefault(arr[right], 0)+1);
+            while(map.size() > 2){
+                map.put(arr[left], map.get(arr[left])-1);
+                if(map.get(arr[left])==0){
+                    map.remove(arr[left]);
                 }
+                left++;
             }
-             if(map.size()==1)return map.get(arr[0]);
-             else  return ans;
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
     }
 }
