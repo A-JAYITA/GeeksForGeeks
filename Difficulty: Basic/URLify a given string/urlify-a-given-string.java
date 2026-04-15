6f752be0b08d6cf6a446f22b973efd0694d14250
Atyam Jayita <jayita.atyam1808@gmail.com>
2026-04-15 20:40:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    String URLify(String s) {
+        String st=" ";
+        for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(ch ==' ')st+="%20";
+        else
+        st+=ch;
+    }
+        return st;
+    }
+}
