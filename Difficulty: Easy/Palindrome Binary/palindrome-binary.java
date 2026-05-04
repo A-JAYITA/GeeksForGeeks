c7a41e1630257ed20536fe93d3ca497e97aff56b
Atyam Jayita <jayita.atyam1808@gmail.com>
2026-05-04 16:21:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Binary/palindrome-binary.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBinaryPalindrome(int n) {
+       String bin = Integer.toBinaryString(n);
+       int l= 0;
+       int r = bin.length() - 1;
+       while( l < r){
+           if(bin.charAt(l) != bin.charAt(r)){
+               return false;
+           }
+           l++;
+           r--;
+       }
+       return true;
+    }
+}
