57a4d4b0ba2f18c1774b744ea375bf195105cf92
Atyam Jayita <jayita.atyam1808@gmail.com>
2026-06-08 12:35:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Nodes with Greater on Right/delete-nodes-with-greater-on-right.java
@@ -0,0 +1,23 @@
+/* Structure of linked list node
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+class Solution {
+    Node compute(Node head) {
+         if(head.next == null) 
+            return head;
+        Node tp = compute(head.next);
+        if(tp.data > head.data)
+            return tp;
+        head.next = tp;
+        return head;
+    }
+}
